6000ddb3860ef6fdab76a6f200902c9b93c50480
Ryan Etten <retten21127@columbushs.com>
2020-11-19 16:58:37 -0500

    video 3.21

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,10 @@
-// while loops
-const names = ['shaun', 'mario', 'luigi'];
-// let i = 0;
+// do while loops
 
-// while(i < 5){
-//   console.log('in loop: ', i); 
-//   i++
-// }
 
-let i = 0;
-while(i < names.length){
-  console.log(names[i]);
+let i = 5;
+
+do {
+  console.log('val of i is: ', i);
   i++;
 }
+while(i < 5)
